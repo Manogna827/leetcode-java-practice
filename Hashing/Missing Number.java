5100af9d5819f9b58ea5cc3d9fe2fadac465b44c
Manogna Thogarla <manognathogarla7@gmail.com>
2026-04-12 18:30:58 +0530

    Create Missing Number.java

---
+++ Hashing/Missing Number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int values:nums){
+            set.add(values);
+        }
+        for(int i=0;i<=nums.length;i++){
+            if(!set.contains(i))
+            return i;
+        }
+        return -1;
+    }
+}
